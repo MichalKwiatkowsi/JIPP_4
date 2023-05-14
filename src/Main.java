4f206826bdf7af2a2aeca4d5c42addbd9bedf62b
Michał Kwiatkowski <92642665+MichalKwiatkowsi@users.noreply.github.com>
2023-05-14 19:46:57 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Cars> carsArrayList = new ArrayList<>();
+
+        carsArrayList.add(new Porche("911"));
+        carsArrayList.add(new Mercedes("class A"));
+        carsArrayList.add(new Toyota("Corolla"));
+        carsArrayList.add(new Ford("Mustang"));
+
+        for (Cars cars : carsArrayList) {
+            cars.description();
+        }
+    }
 }
